88bd49a0ad10b9a22bc572b6d5abb7b1a611e321
kamini892 <85407957+kamini892@users.noreply.github.com>
2021-06-06 20:09:19 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return {
+    var currYear = new Date().getFullYear();
+    <footer>
+    <p>Copyright @{currYear</p>
+    </footer>
+  };
+}
+
+export default Footer;
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from"react-dom";
+import APP from "./APP";
+
+ReactDom.Render(
+  <div>
+  <APP />
+  </div>,
+  document.getElementById("root")
+);
